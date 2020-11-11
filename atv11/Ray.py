53e57d21736bec3f6894e8a3c9d408b492d3a1cd
Guilherme <guilherme_arai19@hotmail.com>
2020-11-10 21:28:26 -0300

    added atv10 && started atv11

---
+++ atv11/Ray.py
@@ -0,0 +1,9 @@
+from Point import *
+
+
+class Ray:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return f"Ray()"
---
+++ atv11/main.py
@@ -0,0 +1,8 @@
+from Sphere import *
+from Point import *
+from Ray import *
+import cv2 as cv
+
+if __name__ == "__main__":
+    p = Point(2, 3)
+    s = Sphere(p, 3)
